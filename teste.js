dfce025e84fe45de9d009a431fab866f38d7b00a
Paul Pessoa <paulmspessoa@gmail.com>
2022-01-31 00:48:54 -0300

    teste de uma funcao com array

--- teste.js
+++ teste.js
@@ -1,1 +1,17 @@
-console.log("Oi");
+/* comentários
+var y = 1;
+var x = 1;
+
+Digite CTRL + /
+
+*/
+
+//funcoes
+var a = 0;
+var n = 0;
+
+function soma (a, b){
+    return a + b; // 8
+    console.log(a+b);
+}
+soma(3, 5);
