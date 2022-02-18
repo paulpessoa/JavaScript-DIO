833dcd2e6e21bb359bb4fe07027fed9860e5e2ae
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2022-02-17 23:55:32 -0300

    funcao compara numeros

---
+++ operadores-js/script.js
@@ -0,0 +1,29 @@
+let num1 = '';
+let num2 = '';
+let soma = '';
+
+/*funcao fazTudo (num1, num2) {
+  const frase1 = comparaNumero(num1, num2);
+  const frase2 = maiorMenor(num1, num2);
+
+  return `${frase1} ${frase2}`;
+}*/
+
+
+
+function comparaNumero (num1, num2) {
+  iguais = ''; 
+  if (num1 === num2) {
+      console.log("Os números " + num1 + " e " + num2 + "" + iguais + " são iguais.");
+        }
+      
+    else {
+        iguais = 'não';
+      console.log("Os números " + num1 + " e " + num2 + " " + iguais + " são iguais.");
+    };    
+}
+
+comparaNumero(1, 2);
+
+
+
